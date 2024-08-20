f6671c6d546038605a9f60d3b623b711c208d19b
keithpohl82 <keith.pohl82@gmail.com>
2024-08-19 20:34:15 -0500

    819at834pm

--- scriptHelper.js
+++ scriptHelper.js
@@ -5,6 +5,7 @@ require('cross-fetch/polyfill');
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.   
                 
+    
                  document.innerHTML = `
                  <h2>Mission Destination</h2>
                  <ol>
